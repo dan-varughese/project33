c26c86c03a14a8cfb7e8559879020c5dbc08e8ff
dan varughese <51270390+dan-varughese@users.noreply.github.com>
2020-12-07 16:05:04 -0500

    Add files via upload

---
+++ Particle.js
@@ -0,0 +1,21 @@
+class Particles {
+
+    constructor(x, y) {
+        var options = {
+            isStatic: false
+        }
+
+        this.body = Bodies.circle(x, y, 5, options);
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        ellipseMode(CENTER);
+        fill(this.color);
+        ellipse(pos.x, pos.y, 10, 10);
+    }
+
+
+}
---
+++ Plinko.js
@@ -0,0 +1,20 @@
+class Plinko {
+
+    constructor(x, y) {
+        var options = {
+            isStatic: true
+        }
+
+        this.body = Bodies.circle(x, y, 5, options);
+        World.add(world, this.body);
+    }
+
+    display() {
+        var pos = this.body.position;
+        ellipseMode(CENTER);
+        fill("white");
+        ellipse(pos.x, pos.y, 10, 10);
+    }
+
+
+}
